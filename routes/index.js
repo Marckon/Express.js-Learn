e724c02d490971e870b9f1033228ee5fc0d164d0
LCZ <mystric@foxmail.com>
2018-12-07 14:41:14 +0800

    💄 learn request params

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/books/:bookId',(req,res)=>res.send(`your bookId is ${req.params.bookId}`));
+router.get('/flight/:from-:to',(req,res)=>res.send(`you're flying from ${req.params.from} to ${req.params.to}`));
 
 module.exports = router;
