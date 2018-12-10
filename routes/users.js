d9f57aab3e4639f1ddf19257e912a1a78ce126ce
LCZ <mystric@foxmail.com>
2018-12-10 14:20:34 +0800

    💄use application-level middleware

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+//vip or not
+router.get('/user/:id',
+    (req, res, next) => {
+        req.params.id === '0' ? next('route') : next();
+    },
+    (req, res, next) => {
+        res.send('you are <b>Ordinary User</b>')
+    }
+);
+router.get('/user/:id', (req, res, next) => {
+    res.send('cool! Welcome my <b>VIP user</b>')
 });
 
 module.exports = router;
